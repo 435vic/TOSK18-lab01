4cc180a2e13ff97604ca11403689462b27db7942
Victor Quintana <435victorjavier@gmail.com>
2025-11-07 13:37:47 +0100

    repo structure

---
+++ lab1-03/lab1-03-loops.c
@@ -0,0 +1,17 @@
+#include "stdio.h"
+
+int main() {
+    for (int i = 0; i < 11; i++) {
+        if (i < 10) {
+            printf("%d/", i);
+        } else {
+            printf("%d\n", i);
+        }
+    }
+
+    
+    for (int i = 27; i > 
+
+    return 0;
+}
+
